4da309d83e56d80cdde1b47c97bdaf828ff85543
Eric Posas <ericposas@outlook.com>
2021-09-06 01:59:30 -0500

    planning out SharesController feature

---
+++ src/Controllers/SharesController.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace PHPapp\Controllers;
+
+/**
+ * Description of SharesController
+ *
+ * @author webdev00
+ */
+class SharesController
+{
+    
+    ////////////////////
+    //                //
+    //  Plan Routes   //
+    //                //
+    ////////////////////
+    
+    # email notification service of a Share to another User
+    
+    # phone/mobile notification service of a Share to another User
+    
+    # get all Shares belonging to a User
+    
+    # get all shared Lists belonging to a User (recipient)
+    
+    # get all of a User's Lists that he/she has sent (shared) to other Users (sender)
+    
+    # get a single Share by id
+    
+}
